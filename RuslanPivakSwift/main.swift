29fc3bd0c740e7034a13e39001dedad48e53a038
Julian Exenberger <julianexenberger@julians-macbook-pro.local>
2017-01-14 10:38:18 +0200

    Initial Commit

---
+++ RuslanPivakSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RuslanPivakSwift
+//
+//  Created by Julian Exenberger on 2017/01/14.
+//  Copyright © 2017 Julian Exenberger. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
